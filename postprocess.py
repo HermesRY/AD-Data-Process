1cb5d0a123e0dab995b23b9f2883b6d002a2e205
johnP-Li <847041172@qq.com>
2023-07-13 14:49:48 +0800

    bugs fixing

--- postprocess.py
+++ postprocess.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import numpy as np
+import pandas as pd
 from nx_samplers import Pool
 from multiprocessing import Process
 
@@ -12,7 +13,7 @@ depth_frames = 16
 depth_shape = (16, 112, 112)
 radar_shape = (20, 2, 16, 32, 16)
 audio_shape = (20, 87)
-max_workers = 32
+max_workers = 8
 
 def process_video(path):
     cap = cv2.VideoCapture(path)
@@ -50,12 +51,12 @@ def process_video(path):
     return features
 
 def _make_modality_path(root):
-    modal = ['audio', 'dehtp', 'radar']
+    modal = ['audio', 'depth', 'radar']
     for m in modal:
         if not os.path.exists(os.path.join(root, m)):
             os.makedirs(os.path.join(root, m))
 
-def _process_single_time(source_root, target_root, timestamp):
+def _process_single_time(source_root, target_root, timestamp, id):
     try:
         _make_modality_path(target_root)
         audio_path = os.path.join(source_root, 'audio')
@@ -64,15 +65,15 @@ def _process_single_time(source_root, target_root, timestamp):
         audio_file = np.load(os.path.join(audio_path, timestamp+'.npy'))
         radar_file = np.load(os.path.join(radar_path, timestamp+'.npy'))
         depth_features = process_video(os.path.join(depth_path, timestamp+'.mp4'))
-        if audio_file.shape == audio_shape and radar_file.shape == radar_shape and depth_features == depth_shape:
-            np.save(os.path.join(target_root, 'audio', timestamp+'.npy'), audio_file)
-            np.save(os.path.join(target_root, 'depth', timestamp+'.npy'), depth_features)
-            np.save(os.path.join(target_root, 'radar', timestamp+'.npy'), radar_file)
+        if audio_file.shape == audio_shape and radar_file.shape == radar_shape and depth_features.shape == depth_shape:
+            np.save(os.path.join(target_root, 'audio', str(id)+'.npy'), audio_file)
+            np.save(os.path.join(target_root, 'depth', str(id)+'.npy'), depth_features)
+            np.save(os.path.join(target_root, 'radar', str(id)+'.npy'), radar_file)
     except Exception as e:
         print(f"Failed to process {timestamp} data under {source_root}. Error message {e}")
 
 
-def _process_single_subject(cur_root, target_root, yolo=None):
+def _process_single_subject(cur_root, target_root, yolo=None, workers=8):
     cur_depth_ts = [item.split('.')[0] for item in os.listdir(os.path.join(cur_root, 'depth')) if item.endswith('.mp4')]
     cur_radar_ts = [item.split('.')[0] for item in os.listdir(os.path.join(cur_root, 'radar')) if item.endswith('.npy')]
     cur_audio_ts = [item.split('.')[0] for item in os.listdir(os.path.join(cur_root, 'audio')) if item.endswith('.npy')]
@@ -80,13 +81,15 @@ def _process_single_subject(cur_root, target_root, yolo=None):
     common_ts = audio_ts.intersection(depth_ts, radar_ts)
     if yolo is not None:
         common_ts = common_ts.intersection(set(yolo))
-    process_list = []
-    for timestamp in common_ts:
-        process_list.append(Process(target=_process_single_time, args=(cur_root, target_root, timestamp)))
-    for p in process_list:
-        p.start()
-    for p in process_list:
-        p.join()
+    common_ts = list(common_ts)
+    df = pd.DataFrame({'id': np.arange(len(common_ts)), 'timestamp': common_ts})
+    df.to_csv(os.path.join(target_root, "timestamp.csv"), index=False)
+    
+    with Pool(workers) as pool:
+        for id, timestamp in enumerate(common_ts):
+            pool.apply_async(_process_single_time, args=(cur_root, target_root, timestamp, id))
+        pool.close()
+        pool.join()
     
 
 def run(num_workers):
@@ -102,8 +105,8 @@ def run(num_workers):
             target_label_dir = os.path.join(target_path, id, 'label')
             cur_unlabel_dir = os.path.join(data_path, id, 'unlabel')
             target_unlabel_dir = os.path.join(target_path, id, 'unlabel')
-            pool.apply(_process_single_subject, args=(cur_label_dir, target_label_dir, yolo_ts))
-            pool.apply(_process_single_subject, args=(cur_unlabel_dir, target_unlabel_dir))
+            pool.apply_async(_process_single_subject, args=(cur_label_dir, target_label_dir, yolo_ts))
+            pool.apply_async(_process_single_subject, args=(cur_unlabel_dir, target_unlabel_dir))
         pool.close()
         pool.join()
 
